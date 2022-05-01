7689bb36a836b250317a7c3183597bf076465d35
Dries Cruyskens <dries.cruyskens@gmail.com>
2022-05-01 19:56:22 +0200

    intialised canvas, imported packages

--- www/index.js
+++ www/index.js
@@ -1,3 +1,13 @@
 import * as wasm from "differential-growth";
+import * as dat from 'dat.gui';
+import * as paper from "paper";
+import { saveAs } from "file-saver";
 
-wasm.greet();
+const gui = new dat.GUI();
+
+// wasm.greet();
+
+// Only executed our code once the DOM is ready.
+window.onload = function() {
+    paper.setup(document.getElementById("paper-canvas"));
+}
