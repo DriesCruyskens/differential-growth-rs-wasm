5ef95854f7e068fba41b423248ff22720e917d5e
Dries Cruyskens <dries.cruyskens@gmail.com>
2022-06-15 23:36:05 +0200

    Switched from micromath to nalgebra

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,7 @@ mod utils;
 
 use std::ops::{AddAssign, MulAssign};
 
-use micromath::{
-    vector::{Vector, Vector2d},
-    *,
-};
+use nalgebra::Vector2;
 use utils::{generate_points_of_circle, set_panic_hook};
 use wasm_bindgen::prelude::*;
 
@@ -42,17 +39,12 @@ pub fn init(
 ) -> Box<[f32]> {
     set_panic_hook();
 
-    let line: Line = Line::new(
-        F32 { 0: origin_x },
-        F32 { 0: origin_y },
-        amount_of_points,
-        F32 { 0: radius },
-    );
+    let line: Line = Line::new(origin_x, origin_y, amount_of_points, radius);
 
     return line
         .nodes
         .into_iter()
-        .map(|x| x.position.x.0)
+        .map(|node| node.position.x)
         .collect::<Vec<f32>>()
         .into_boxed_slice();
 }
@@ -62,66 +54,43 @@ struct Line {
 }
 
 impl Line {
-    pub fn new(origin_x: F32, origin_y: F32, amount_of_points: usize, radius: F32) -> Line {
+    pub fn new(origin_x: f32, origin_y: f32, amount_of_points: usize, radius: f32) -> Line {
         let nodes: Vec<Node> =
             generate_points_of_circle(origin_x, origin_y, amount_of_points, radius)
                 .into_iter()
-                .map(|point: Vector2d<F32>| Node::new(point))
+                .map(|point: Vector2<f32>| Node::new(point))
                 .collect();
         Line { nodes }
     }
 }
 
 struct Node {
-    position: Vector2d<F32>,
-    velocity: Vector2d<F32>,
-    acceleration: Vector2d<F32>,
-    max_force: F32,
-    max_speed: F32,
+    position: Vector2<f32>,
+    velocity: Vector2<f32>,
+    acceleration: Vector2<f32>,
+    max_force: f32,
+    max_speed: f32,
 }
 
 impl Node {
-    pub fn new(position: Vector2d<F32>) -> Node {
+    pub fn new(position: Vector2<f32>) -> Node {
         Node {
             position,
-            velocity: Vector2d {
-                x: F32 { 0: 0.0 },
-                y: F32 { 0: 0.0 },
-            },
-            acceleration: Vector2d {
-                x: F32 { 0: 0.0 },
-                y: F32 { 0: 0.0 },
-            },
-            max_speed: F32 { 0: 2.0 },
-            max_force: F32 { 0: 0.2 },
+            velocity: Vector2::new(0.0, 0.0),
+            acceleration: Vector2::new(0.0, 0.0),
+            max_speed: 2.0,
+            max_force: 0.03,
         }
     }
 
-    pub fn applyForce(&mut self, force: Vector2d<F32>) {
-        self.acceleration = self.acceleration + force;
+    pub fn apply_force(&mut self, force: Vector2<f32>) {
+        self.acceleration.add_assign(force);
     }
 
     pub fn update(&mut self) {
-        self.velocity = self.velocity + self.acceleration;
-        self.velocity.limit(self.max_speed);
-        self.position = self.position + self.velocity;
-        self.acceleration.mul_assign(F32 { 0: 0.0 });
-    }
-}
-
-trait Limitable {
-    fn limit(&mut self, max: F32);
-}
-
-// Limiting the vector's magnitude by normalising and multiplying by max.
-impl Limitable for Vector2d<F32> {
-    fn limit(&mut self, max: F32) {
-        let magnitude: f32 = self.magnitude();
-        if magnitude > max {
-            self.x = self.x / magnitude;
-            self.y = self.y / magnitude;
-            self.x = self.x * max;
-            self.y = self.y * max;
-        }
+        self.velocity.add_assign(self.acceleration);
+        self.velocity = self.velocity.cap_magnitude(self.max_speed);
+        self.position.add_assign(self.velocity);
+        self.acceleration.mul_assign(0.0);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 use std::f64::consts::PI;
 
 use iter_num_tools::{lin_space, LinSpace};
-use micromath::{vector::Vector2d, F32};
+use nalgebra::Vector2;
 
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
@@ -15,26 +15,24 @@ pub fn set_panic_hook() {
 }
 
 pub fn generate_points_of_circle(
-    origin_x: F32,
-    origin_y: F32,
+    origin_x: f32,
+    origin_y: f32,
     amount_of_points: usize,
-    radius: F32,
-) -> Vec<Vector2d<F32>> {
-    let mut points: Vec<Vector2d<F32>> = Vec::new();
+    radius: f32,
+) -> Vec<Vector2<f32>> {
+    let mut points: Vec<Vector2<f32>> = Vec::new();
 
-    let h: F32 = origin_x;
-    let k: F32 = origin_y;
+    let h: f32 = origin_x;
+    let k: f32 = origin_y;
 
     let two_pi: f32 = (2.0 * PI as f32).round();
 
     let lin_space: LinSpace<f32> = lin_space(0.0..=two_pi, amount_of_points);
 
-    for i in lin_space {
-        let theta: F32 = F32 { 0: i };
-        let x: F32 = h + radius * F32::cos(theta);
-        let y: F32 = k + radius * F32::sin(theta);
-        let point: Vector2d<F32> = Vector2d { x, y };
-        points.push(point);
+    for theta in lin_space {
+        let x: f32 = h + radius * f32::cos(theta);
+        let y: f32 = k + radius * f32::sin(theta);
+        points.push(Vector2::new(x, y));
     }
 
     return points;
