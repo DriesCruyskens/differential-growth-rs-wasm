a3497022eb38c8cbec7dce39e83cc92aa8878bb5
Dries Cruyskens <dries.cruyskens@gmail.com>
2022-06-20 22:50:26 +0200

    Add FPS counter

--- www/index.js
+++ www/index.js
@@ -7,7 +7,7 @@ const gui = new dat.GUI();
 
 // wasm.greet();
 
-let totalSteps = 1000;
+let totalSteps = 300;
 let currentStep = 0;
 
 let radius = 10;
@@ -44,6 +44,8 @@ window.onload = function () {
 
     // Loop
     function animationLoop() {
+        fps.render();
+
         if (currentStep > totalSteps) {
             return
         }
@@ -78,3 +80,48 @@ function getSegments(line) {
 
     return segments
 }
+
+const fps = new class {
+    constructor() {
+      this.fps = document.getElementById("fps");
+      this.frames = [];
+      this.lastFrameTimeStamp = performance.now();
+    }
+  
+    render() {
+      // Convert the delta time since the last frame render into a measure
+      // of frames per second.
+      const now = performance.now();
+      const delta = now - this.lastFrameTimeStamp;
+      this.lastFrameTimeStamp = now;
+      const fps = 1 / delta * 1000;
+  
+      // Save only the latest 100 timings.
+      this.frames.push(fps);
+      if (this.frames.length > 100) {
+        this.frames.shift();
+      }
+  
+      // Find the max, min, and mean of our 100 latest timings.
+      let min = Infinity;
+      let max = -Infinity;
+      let sum = 0;
+      for (let i = 0; i < this.frames.length; i++) {
+        sum += this.frames[i];
+        min = Math.min(this.frames[i], min);
+        max = Math.max(this.frames[i], max);
+      }
+      let mean = sum / this.frames.length;
+  
+      // Render the statistics.
+      this.fps.textContent = `
+  Frames per Second:
+           latest = ${Math.round(fps)}
+  avg of last 100 = ${Math.round(mean)}
+  min of last 100 = ${Math.round(min)}
+  max of last 100 = ${Math.round(max)}
+
+Amount of Steps = ${currentStep}
+  `.trim();
+    }
+  };
